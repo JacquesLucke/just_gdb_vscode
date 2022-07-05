58b4b95304603e953b8e93f16332458d8923e5cc
Jacques Lucke <mail@jlucke.com>
2022-07-05 21:19:35 +0200

    initial test of starting gdb

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as child_process from 'child_process';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+
+let gdb_process: child_process.ChildProcessWithoutNullStreams | null = null;
+
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "direct-gdb" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('direct-gdb.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Direct GDB!');
+		if (gdb_process === null) {
+			gdb_process = child_process.spawn("gdb", ["/home/jacques/Documents/test_c_debug/a.out"])
+			gdb_process.stdout.on('data', on_gdb_stdout);
+			gdb_process.on('close', on_gdb_close);
+		}
+		else {
+			gdb_process.stdin.write("run\n");
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
+
+function on_gdb_stdout(data: Buffer) {
+	console.log(data.toString());
+}
+
+function on_gdb_close(status_code: number) {
+	console.log('Closed');
+}
