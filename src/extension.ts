b5056c7be6a662d26023fbfc7ffdbc65aa47caf7
Jacques Lucke <mail@jlucke.com>
2022-07-05 22:15:51 +0200

    more testing

--- src/extension.ts
+++ src/extension.ts
@@ -5,26 +5,64 @@ import * as child_process from 'child_process';
 let gdb_process: child_process.ChildProcessWithoutNullStreams | null = null;
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('direct-gdb.helloWorld', () => {
-		if (gdb_process === null) {
-			gdb_process = child_process.spawn("gdb", ["/home/jacques/Documents/test_c_debug/a.out"])
-			gdb_process.stdout.on('data', on_gdb_stdout);
-			gdb_process.on('close', on_gdb_close);
-		}
-		else {
-			gdb_process.stdin.write("run\n");
-		}
-	});
 
-	context.subscriptions.push(disposable);
+	const commands: [string, any][] = [
+		['direct-gdb.startGDB', COMMAND_startGDB],
+		['direct-gdb.run', COMMAND_run],
+		['direct-gdb.pause', COMMAND_pause],
+		['direct-gdb.playground', COMMAND_playground],
+	];
+
+	for (const item of commands) {
+		context.subscriptions.push(vscode.commands.registerCommand(item[0], item[1]));
+	}
 }
 
 export function deactivate() { }
+
+function COMMAND_startGDB() {
+	if (gdb_process !== null) {
+		return;
+	}
+	gdb_process = child_process.spawn("gdb", ["/home/jacques/Documents/test_c_debug/a.out"])
+	gdb_process.stdout.on('data', on_gdb_stdout);
+	gdb_process.on('close', on_gdb_close);
+}
+
+function COMMAND_run() {
+	if (gdb_process === null) {
+		return;
+	}
+	// gdb_process.stdin.write("b main.cc:8\n");
+	gdb_process.stdin.write("run\n");
+}
+
+function COMMAND_pause() {
+	if (gdb_process === null) {
+		return;
+	}
+	gdb_process.kill('SIGINT');
+	gdb_process.stdin.write("p i\n");
+}
+
+let currentLine = 1;
+
+const currentLineDecorationType = vscode.window.createTextEditorDecorationType({
+	isWholeLine: true,
+	backgroundColor: new vscode.ThemeColor("editor.stackFrameHighlightBackground"),
+	rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed,
+});
+
+function COMMAND_playground() {
+	const editor = vscode.window.visibleTextEditors[0];
+	editor.setDecorations(currentLineDecorationType, [new vscode.Range(currentLine, 0, currentLine, 100000)]);
+	currentLine++;
+}
 
 function on_gdb_stdout(data: Buffer) {
 	console.log(data.toString());
 }
 
 function on_gdb_close(status_code: number) {
-	console.log('Closed');
+	gdb_process = null;
 }
