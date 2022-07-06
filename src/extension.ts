234227181bdff78579ab108088a547e8c15d71b6
Jacques Lucke <mail@jlucke.com>
2022-07-06 19:26:39 +0200

    cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,97 @@
 import * as vscode from 'vscode';
 import * as child_process from 'child_process';
+import * as path from 'path';
+
+class DebugSession {
+	gdbBinaryPath: string;
+	gdbArgs: string[];
+	terminalWriteEmitter: vscode.EventEmitter<string>;
+	terminal: vscode.Terminal;
+	gdbProcess: child_process.ChildProcess | null = null;
+	currentTerminalLine: string = "";
+
+	constructor(gdbBinaryPath: string, args: string[], terminalName: string) {
+		this.gdbBinaryPath = gdbBinaryPath;
+		this.gdbArgs = args;
+		this.terminalWriteEmitter = new vscode.EventEmitter<string>();
+		this.terminal = vscode.window.createTerminal({
+			name: terminalName,
+			pty: {
+				onDidWrite: this.terminalWriteEmitter.event,
+				open: this.onTerminalOpen.bind(this),
+				close: this.onTerminalClose.bind(this),
+				handleInput: this.onUserInputInTerminal.bind(this),
+			}
+		});
+	}
+
+	private onTerminalOpen() {
+		// Only create the process once the terminal is open, so that the
+		// initial output is not ignored by the terminal.
+		this.gdbProcess = child_process.spawn(this.gdbBinaryPath, this.gdbArgs);
+		this.gdbProcess.stdout?.on('data', this.onProcessStdout.bind(this));
+		this.gdbProcess.stderr?.on('data', this.onProcessStderr.bind(this));
+		this.gdbProcess.on('close', this.onProcessClose.bind(this));
+	}
+
+	private onTerminalClose() {
+		this.gdbProcess?.kill();
+	}
+
+	private onUserInputInTerminal(data: string) {
+		if (data.endsWith("\r")) { // Enter
+			this.terminalWriteEmitter.fire("\n\r");
+			this.forwardTextToGDB(this.currentTerminalLine + "\n");
+			this.currentTerminalLine = "";
+			return;
+		}
+		if (data === '\x7f') { // Backspace
+			this.terminalWriteEmitter.fire('\x1b[D');
+			this.terminalWriteEmitter.fire('\x1b[P');
+			this.currentTerminalLine = this.currentTerminalLine.slice(0, -1);
+			return;
+		}
+		if (data === '\t') { // Tab
+			this.forwardTextToGDB(this.currentTerminalLine);
+			this.forwardTextToGDB('\t');
+			return;
+		}
+		this.currentTerminalLine += data;
+		this.terminalWriteEmitter.fire(data);
+	}
+
+	private onProcessStdout(data: Buffer) {
+		this.processOutputToTerminal(data);
+	}
+	private onProcessStderr(data: Buffer) {
+		this.processOutputToTerminal(data);
+	}
+	private onProcessClose() { }
+
+	private processOutputToTerminal(data: Buffer) {
+		const data_str = data.toString();
+		// Todo: Figure out why just replacing \n with \n\r did not work correctly.
+		const lines = data_str.split("\n");
+		for (let i = 0; i < lines.length - 1; i++) {
+			this.terminalWriteEmitter.fire(lines[i] + "\n\r");
+		}
+		this.terminalWriteEmitter.fire(lines[lines.length - 1]);
+		if (data_str.endsWith("\n")) {
+			this.terminalWriteEmitter.fire("\n\r");
+		}
+	}
+
+	forwardTextToGDB(command: string) {
+		this.gdbProcess?.stdin?.write(command);
+	}
+
+	interrupt() {
+		this.gdbProcess?.kill('SIGINT');
+	}
+};
 
 
-let gdb_process: child_process.ChildProcessWithoutNullStreams | null = null;
+let debugSession: DebugSession | null = null;
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -17,115 +106,46 @@ export function activate(context: vscode.ExtensionContext) {
 		context.subscriptions.push(vscode.commands.registerCommand(item[0], item[1]));
 	}
 
-	vscode.window.createTreeView("gdbThreads", { treeDataProvider: new ThreadsViewProvider() });
+	// vscode.window.createTreeView("gdbThreads", { treeDataProvider: new ThreadsViewProvider() });
 }
 
 export function deactivate() { }
 
+const mainDir = path.dirname(__dirname);
+const sourceDir = path.join(mainDir, "src");
+const gdbExtensionPath = path.join(sourceDir, "gdb_extension.py")
+
+
 function COMMAND_startGDB() {
-	if (gdb_process !== null) {
+	if (debugSession !== null) {
 		return;
 	}
 
-
-	const writeEmitter = new vscode.EventEmitter<string>();
-
-	let line = "";
-	const terminal = vscode.window.createTerminal({
-		name: 'My Terminal', pty: {
-			onDidWrite: writeEmitter.event,
-			open: () => {
-				gdb_process = child_process.spawn("gdb", ["/home/jacques/Documents/test_c_debug/a.out"])
-
-				const handle_gdb_output = (data: Buffer) => {
-					const data_str = data.toString();
-					const lines = data_str.split("\n");
-					for (let i = 0; i < lines.length - 1; i++) {
-						writeEmitter.fire(lines[i] + "\n\r");
-					}
-					writeEmitter.fire(lines[lines.length - 1]);
-					if (data_str.endsWith("\n")) {
-						writeEmitter.fire("\n\r")
-					}
-				};
-
-				gdb_process.stdout.on('data', handle_gdb_output);
-				gdb_process.stderr.on('data', handle_gdb_output);
-				gdb_process.on('close', on_gdb_close);
-
-				vscode.debug.onDidChangeBreakpoints((e) => {
-					for (const breakpoint of e.added) {
-						if (breakpoint instanceof vscode.SourceBreakpoint) {
-							if (gdb_process !== null) {
-								const command = "b " + breakpoint.location.uri.fsPath + ":" + breakpoint.location.range.start.line;
-								writeEmitter.fire(command + "\n\r");
-								gdb_process.stdin.write(command + "\n");
-							}
-						}
-					}
-					for (const breakpoint of e.removed) {
-						if (breakpoint instanceof vscode.SourceBreakpoint) {
-							if (gdb_process != null) {
-								const command = "clear " + breakpoint.location.uri.fsPath + ":" + breakpoint.location.range.start.line;
-								writeEmitter.fire(command + "\n\r");
-								gdb_process.stdin.write(command + "\n");
-							}
-						}
-					}
-				});
-			},
-			close: () => { },
-			handleInput: (data: string) => {
-				console.log(data.charCodeAt(0));
-				if (data === '\r') { // Enter
-					writeEmitter.fire('\n\r');
-					gdb_process?.stdin.write(line);
-					gdb_process?.stdin.write('\n');
-					line = '';
-					return;
-				}
-				if (data === '\x7f') { // Backspace
-					writeEmitter.fire('\x1b[D');
-					writeEmitter.fire('\x1b[P');
-					return;
-				}
-				if (data === '\t') {
-					writeEmitter.fire('tab');
-					return;
-				}
-				line += data;
-				writeEmitter.fire(data);
-			},
-		}
-	});
-	terminal.show();
-
-
+	debugSession = new DebugSession("gdb", ["/home/jacques/Documents/test_c_debug/a.out"], "GDB");
+	debugSession.terminal.show();
 }
 
 function COMMAND_run() {
-	if (gdb_process === null) {
+	if (debugSession === null) {
 		return;
 	}
-	// gdb_process.stdin.write("b main.cc:8\n");
-	gdb_process.stdin.write("run\n");
+	debugSession.forwardTextToGDB("run\n");
 }
 
 function COMMAND_pause() {
-	if (gdb_process === null) {
+	if (debugSession === null) {
 		return;
 	}
-	gdb_process.kill('SIGINT');
-	gdb_process.stdin.write("p i\n");
+	debugSession.interrupt();
 }
 
-let currentLine = 1;
+// let currentLine = 1;
 
-const currentLineDecorationType = vscode.window.createTextEditorDecorationType({
-	isWholeLine: true,
-	backgroundColor: new vscode.ThemeColor("editor.stackFrameHighlightBackground"),
-	rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed,
-});
+// const currentLineDecorationType = vscode.window.createTextEditorDecorationType({
+// 	isWholeLine: true,
+// 	backgroundColor: new vscode.ThemeColor("editor.stackFrameHighlightBackground"),
+// 	rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed,
+// });
 
 function COMMAND_playground() {
 	// const editor = vscode.window.visibleTextEditors[0];
@@ -146,29 +166,25 @@ function COMMAND_playground() {
 	// console.log(process.argv0);
 }
 
-function on_gdb_close(status_code: number) {
-	gdb_process = null;
-}
 
+// class ThreadsViewProvider implements vscode.TreeDataProvider<ThreadsViewItem>{
+// 	getTreeItem(element: ThreadsViewItem) {
+// 		return element;
+// 	}
 
-class ThreadsViewProvider implements vscode.TreeDataProvider<ThreadsViewItem>{
-	getTreeItem(element: ThreadsViewItem) {
-		return element;
-	}
+// 	getChildren(element?: ThreadsViewItem): vscode.ProviderResult<ThreadsViewItem[]> {
+// 		if (element) {
+// 			return [];
+// 		}
+// 		else {
+// 			return [new ThreadsViewItem(), new ThreadsViewItem()];
+// 		}
 
-	getChildren(element?: ThreadsViewItem): vscode.ProviderResult<ThreadsViewItem[]> {
-		if (element) {
-			return [];
-		}
-		else {
-			return [new ThreadsViewItem(), new ThreadsViewItem()];
-		}
+// 	}
+// };
 
-	}
-};
-
-class ThreadsViewItem extends vscode.TreeItem {
-	constructor() {
-		super("Hello World");
-	}
-};
+// class ThreadsViewItem extends vscode.TreeItem {
+// 	constructor() {
+// 		super("Hello World");
+// 	}
+// };
