a1ee7f8a4ab3835b9a3442707618669e2b508e9e
Jacques Lucke <mail@jlucke.com>
2022-07-06 18:05:28 +0200

    more testing

--- src/extension.ts
+++ src/extension.ts
@@ -24,9 +24,81 @@ function COMMAND_startGDB() {
 	if (gdb_process !== null) {
 		return;
 	}
-	gdb_process = child_process.spawn("gdb", ["/home/jacques/Documents/test_c_debug/a.out"])
-	gdb_process.stdout.on('data', on_gdb_stdout);
-	gdb_process.on('close', on_gdb_close);
+
+
+	const writeEmitter = new vscode.EventEmitter<string>();
+
+	let line = "";
+	const terminal = vscode.window.createTerminal({
+		name: 'My Terminal', pty: {
+			onDidWrite: writeEmitter.event,
+			open: () => {
+				gdb_process = child_process.spawn("gdb", ["/home/jacques/Documents/test_c_debug/a.out"])
+
+				const handle_gdb_output = (data: Buffer) => {
+					const data_str = data.toString();
+					const lines = data_str.split("\n");
+					for (let i = 0; i < lines.length - 1; i++) {
+						writeEmitter.fire(lines[i] + "\n\r");
+					}
+					writeEmitter.fire(lines[lines.length - 1]);
+					if (data_str.endsWith("\n")) {
+						writeEmitter.fire("\n\r")
+					}
+				};
+
+				gdb_process.stdout.on('data', handle_gdb_output);
+				gdb_process.stderr.on('data', handle_gdb_output);
+				gdb_process.on('close', on_gdb_close);
+
+				vscode.debug.onDidChangeBreakpoints((e) => {
+					for (const breakpoint of e.added) {
+						if (breakpoint instanceof vscode.SourceBreakpoint) {
+							if (gdb_process !== null) {
+								const command = "b " + breakpoint.location.uri.fsPath + ":" + breakpoint.location.range.start.line;
+								writeEmitter.fire(command + "\n\r");
+								gdb_process.stdin.write(command + "\n");
+							}
+						}
+					}
+					for (const breakpoint of e.removed) {
+						if (breakpoint instanceof vscode.SourceBreakpoint) {
+							if (gdb_process != null) {
+								const command = "clear " + breakpoint.location.uri.fsPath + ":" + breakpoint.location.range.start.line;
+								writeEmitter.fire(command + "\n\r");
+								gdb_process.stdin.write(command + "\n");
+							}
+						}
+					}
+				});
+			},
+			close: () => { },
+			handleInput: (data: string) => {
+				console.log(data.charCodeAt(0));
+				if (data === '\r') { // Enter
+					writeEmitter.fire('\n\r');
+					gdb_process?.stdin.write(line);
+					gdb_process?.stdin.write('\n');
+					line = '';
+					return;
+				}
+				if (data === '\x7f') { // Backspace
+					writeEmitter.fire('\x1b[D');
+					writeEmitter.fire('\x1b[P');
+					return;
+				}
+				if (data === '\t') {
+					writeEmitter.fire('tab');
+					return;
+				}
+				line += data;
+				writeEmitter.fire(data);
+			},
+		}
+	});
+	terminal.show();
+
+
 }
 
 function COMMAND_run() {
@@ -54,13 +126,22 @@ const currentLineDecorationType = vscode.window.createTextEditorDecorationType({
 });
 
 function COMMAND_playground() {
-	const editor = vscode.window.visibleTextEditors[0];
-	editor.setDecorations(currentLineDecorationType, [new vscode.Range(currentLine, 0, currentLine, 100000)]);
-	currentLine++;
-}
+	// const editor = vscode.window.visibleTextEditors[0];
+	// editor.setDecorations(currentLineDecorationType, [new vscode.Range(currentLine, 0, currentLine, 100000)]);
+	// currentLine++;
 
-function on_gdb_stdout(data: Buffer) {
-	console.log(data.toString());
+	// const terminal = vscode.window.createTerminal("GDB", "gdb");
+	// terminal.show();
+	// terminal.sendText("r\t\t", false);
+	// setTimeout(() => {
+	// 	terminal.processId.then((value) => {
+	// 		if (value !== undefined) {
+	// 			process.kill(value, 'SIGINT');
+	// 		}
+	// 	})
+	// }, 2000);
+
+	// console.log(process.argv0);
 }
 
 function on_gdb_close(status_code: number) {
